bbe0fb9d170b75de0b54039d0a95eef499df923e
namsi-lia <namsilydia7@gmail.com>
2023-02-20 11:00:42 +0300

    Create models

--- crm/settings.py
+++ crm/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'leads'
+    'leads' #added the new app created this is to ensure that the django project detects every change made.
 ]
 
 MIDDLEWARE = [
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL ='leads.User'
