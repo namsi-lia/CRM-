ac2540c5b70addc4fabfaaeed9fc13069c66c8f3
namsi-lia <namsilydia7@gmail.com>
2023-02-21 11:25:04 +0300

    add models to admin

--- leads/admin.py
+++ leads/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import User, Leads, Agent
+## this imports all the models created on the models file
+
 # Register your models here.
+## register your database schema
+
+admin.site.register(User)
+
--- leads/models.py
+++ leads/models.py
@@ -21,4 +21,4 @@ class Agent (models.Model):
     user =models.OneToOneField(User,on_delete=models.CASCADE)
    
     def __str__(self):
-        return self.user.email
+        return f"{self.first_name} {self.last_name}"
