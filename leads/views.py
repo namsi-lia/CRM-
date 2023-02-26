86df2366ffca7507ca7c9baeb52c3c4023ff506d
namsi-lia <namsilydia7@gmail.com>
2023-02-26 18:27:57 +0300

    Add views and templates

--- leads/views.py
+++ leads/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Leads
 
 # Create your views here.
 
 ##create function homepage
 def home_page(request):
    # return HttpResponse('Welcome to Sales')
-   return render(request ,"leads/home_page.html")
+   leads = Leads.objects.all()
+   context ={
+    "leads":leads
+   }
+   return render(request ,"leads/home_page.html",context )
